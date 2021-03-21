e13e5786f68e69bc654eec4086251da41f7e85ce
rabiya-basri <51847270+rabiya-basri@users.noreply.github.com>
2021-03-21 14:17:57 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
+// Header Component
+const Header = () => (
+  <header>
+    <div className='header-wrapper'>
+      <h1>Welcome to 30 Days Of React</h1>
+      <h2>Getting Started React</h2>
+      <h3>JavaScript Library</h3>
+      <p>Asabeneh Yetayeh</p>
+      <small>Oct 3, 2020</small>
+    </div>
+  </header>
+)
+
 const rootElement = document.getElementById('root')
-
-const heading = 'SUBSCRIBE'
-const description = 'Signup with your email address to recive news and updates'
-const formheading = (
-  <div className="formstyle">
-    <h1>{heading}</h1>
-    <p>{description}</p>
-    <input type="text" placeholder="firstname" />
-    <input type="text" placeholder="lastname" />
-    <input type="text" placeholder="Email" />
-    <button>Submit</button>
-   </div>
- )
-
-ReactDOM.render(formheading,rootElement)
+// we render the JSX element using the ReactDOM package
+ReactDOM.render(<Header />, rootElement)
